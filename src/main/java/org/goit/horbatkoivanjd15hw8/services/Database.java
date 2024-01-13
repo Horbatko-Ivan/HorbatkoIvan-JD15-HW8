944cf56c429a3118425427d82cd543a079f13c5b
Ivan Horbatko <44212827+Horbatko-Ivan@users.noreply.github.com>
2024-01-13 21:46:32 +0100

    HorbatkoIvan-HW8-JD15 / specify a path relative to the current working directory

--- src/main/java/org/goit/horbatkoivanjd15hw8/services/Database.java
+++ src/main/java/org/goit/horbatkoivanjd15hw8/services/Database.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class Database {
 
   public static final String URL_DB
-      = "C:\\Users\\Ivan\\IdeaProjects\\HorbatkoIvan-JD15-HW8\\src\\main\\resources\\db\\hw8";
+      = "./resources/db/hw8";
   private static final Database INSTANCE = new Database();
   private Connection connection;
 
